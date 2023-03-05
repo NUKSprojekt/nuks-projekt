e843fac6f6213362110f170735b76cf6c47619ba
andrazmen <and.menegatti@gmail.com>
2023-03-05 19:22:36 +0000

    Uploading project files

---
+++ Vaja2/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String 
+
+engine = create_engine("sqlite:///tododatabase.db") # engine poganja DB
+
+Base = declarative_base() # vrne class; baza za ta class, ki predstavlja tabelo v databazi
+
+# class toDO predstavlja sql tabelo todos
+# atributi v classu predstavljajo columns z imenom in tipom
+# objekti v classu predstavljajo vrstico v tabeli
+
+
+class ToDO(Base):
+    __tablename__ = "todos" 
+    id = Column(Integer, primary_key=True) # id todos ukazov, tipa int, predstavlja primary key
+    task = Column(String(50)) # taski so tipa string
---
+++ Vaja2/main.py
@@ -0,0 +1,49 @@
+from typing import Union
+from fastapi import FastAPI
+from database import Base, engine, ToDO
+import schemas
+from sqlalchemy.orm import Session # orm povezuje objekte (maps) v kodi z database tabele (relations)
+
+
+Base.metadata.create_all(engine)
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    """
+    Default API call
+    """
+    return {"TODO app"}
+
+
+@app.post("/add")
+def create_todo(todo: schemas.ToDoTask):
+    session = Session(bind = engine, expire_on_commit=False)
+    tododb = ToDO(task = todo.task)
+
+    session.add(tododb)
+    session.commit()
+    id = tododb.id
+    session.close()
+    return f"Created new todo with id: {id}"
+
+    return "Create TODO"
+
+@app.get("/get/{id}")
+def read_todo(id: int):
+    return "Read todo item with id {id}"
+
+@app.put("/change/{id}")
+def change_todo(id: int):
+    return "Read todo item with id {id}"
+
+@app.delete("/delete/{id}")
+def change_todo(id: int):
+    return "Delete todo item with id {id}"
+
+@app.get("list")
+def read_todo_list():
+    return "All todos"
